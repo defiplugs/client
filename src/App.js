695544dc3495d6c3da67b44328670f7fb55e7a72
defiplugs <defiplugs@protonmail.com>
2021-03-03 15:40:23 +0800

    change mod

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>DEFIPLUGS TES</p>
+        <p>DEFIPLUGS TESt</p>
       </header>
     </div>
   );
