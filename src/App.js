64caccd1b47815d2916601ca470eace8655908cf
defiplugs <defiplugs@protonmail.com>
2021-03-03 12:23:36 +0800

    create dev branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>DEFIPLUGS</p>
+        <p>DEFIPLUGS TEST</p>
       </header>
     </div>
   );
