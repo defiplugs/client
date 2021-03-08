bcd7471cc5d176691c5cbf678e144530b5a72ea5
defiplugs <defiplugs@protonmail.com>
2021-03-08 19:53:59 +0800

    detect user address

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ function App() {
   if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
   }
-  const initiateWallet = () => {
-    window.ethereum.request({ method: 'eth_requestAccounts' });
+
+  const initiateWallet = async () => {
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+    const account = accounts[0];
+    console.log(account);
   };
+
   return (
     <div className="App">
       <header className="App-header">
