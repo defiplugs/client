8bb7ff4ab31f9825d24f170213975033c819c9b5
defiplugs <defiplugs@protonmail.com>
2021-03-11 11:41:45 +0800

    read user account

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [account, setAccount] = useState('');
+
   if (typeof window.ethereum !== 'undefined') {
-    console.log('MetaMask is installed!');
+    // console.log('MetaMask is installed!');
   }
+
+  useEffect(() => {
+    //console.log(window.ethereum.isConnected());
+    // if (window.ethereum.isConnected()) {
+    //   console.log('conn');
+    //   setAccount(window.ethereum.selectedAddress);
+    // }
+    // console.log(window.ethereum.isConnected());
+    window.ethereum
+      .request({
+        method: 'eth_requestAccounts',
+      })
+      .then((account) => {
+        setAccount(account);
+      });
+  }, []);
 
   const initiateWallet = async () => {
     const accounts = await window.ethereum.request({
       method: 'eth_requestAccounts',
     });
     const account = accounts[0];
-    console.log(account);
+    setAccount(account);
   };
+
+  console.log(window.ethereum.selectedAddress);
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={initiateWallet}>CONNECT WALLET</button>
+        {!account && <button onClick={initiateWallet}>CONNECT WALLET</button>}
+
+        <p>{account}</p>
       </header>
     </div>
   );
